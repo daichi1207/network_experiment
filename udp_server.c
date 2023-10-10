9f784f73768722b994b5cd5a2f0531335b34bed4
daichi1207 <daichiyashima@gmail.com>
2023-10-10 14:02:09 +0900

    fix: avoid buffer overflow

--- udp_server.c
+++ udp_server.c
@@ -7,21 +7,21 @@
 /*                                                                  */
 
 #include "icslab2_net.h"
+// adjust this for buffer overflow
 #define MAX_EVENTS 5
+#define SLEEPTIME 1000
 
 int main(int argc, char **argv) {
   int sock;                      /* ソケットディスクリプタ */
   struct sockaddr_in serverAddr; /* サーバ＝自分用アドレス構造体 */
   struct sockaddr_in clientAddr; /* クライアント＝相手用アドレス構造体 */
-  unsigned int port = UDP_SERVER_PORT;
-  int addrLen;       /* clientAddrのサイズ */
-  char buf[BUF_LEN]; /* 受信バッファ */
-  int n;             /* 受信バイト数 */
+  int addrLen;                   /* clientAddrのサイズ */
+  char buf[BUF_LEN];             /* 受信バッファ */
+  int n;                         /* 受信バイト数 */
   char *filename;
   int fd;
   int epfd;
   int nfds;
-  int rev_cnt = 0;
 
   struct epoll_event ev, events[MAX_EVENTS];
 
@@ -44,16 +44,16 @@ int main(int argc, char **argv) {
   }
 
   /* STEP 2: クライアントからの要求を受け付けるIPアドレスとポートを設定する */
-  memset(&serverAddr, 0, sizeof(serverAddr)); /* ゼロクリア */
-  serverAddr.sin_family = AF_INET;            /* Internetプロトコル */
-  serverAddr.sin_port = htons(port);          /* 待ち受けるポート */
+  memset(&serverAddr, 0, sizeof(serverAddr));   /* ゼロクリア */
+  serverAddr.sin_family = AF_INET;              /* Internetプロトコル */
+  serverAddr.sin_port = htons(UDP_SERVER_PORT); /* 待ち受けるポート */
   serverAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* どのIPアドレス宛でも */
+  /* STEP 3:ソケットとアドレスをbindする */
   if (bind(sock, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
     perror("bind");
     return 1;
   }
 
-  // create epoll
   memset(&ev, 0, sizeof(ev));
   ev.events = EPOLLIN;
   ev.data.fd = sock;
@@ -63,32 +63,47 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  // add socket to epoll
   if (epoll_ctl(epfd, EPOLL_CTL_ADD, sock, &ev) != 0) {
     perror("epoll_ctl 1");
     return 1;
   }
 
-  for (;;) {
-    printf("wait\n");
-    // addrLen = sizeof(clientAddr);
-    // n = recvfrom(sock, buf, BUF_LEN, 0, (struct sockaddr *)&clientAddr,
-    //              (socklen_t *)&addrLen);
-    // if (n < 0) {
-    //   perror("recvfrom");
-    //   break;
-    // }
-    // printf("received data\n");
+  for (;;) { /* 無限ループ */
+    /* STEP 4: クライアントからのデータグラムを受けとる */
+    printf("waiting connection...\n");
+    addrLen = sizeof(clientAddr);
+    n = recvfrom(sock, buf, BUF_LEN, 0, (struct sockaddr *)&clientAddr,
+                 (socklen_t *)&addrLen);
+    if (n < 0) {
+      perror("recvfrom");
+      break;
+    }
+    printf("received data\n");
+
+    /* 受信パケットの送信元IPアドレスとポート番号を表示 */
+    addr.s_addr = clientAddr.sin_addr.s_addr;
+    printf("received from :  ip address: %s, ", inet_ntoa(addr));
+    printf("port#: %d\n", ntohs(clientAddr.sin_port));
+
+    fd = open(filename, O_RDONLY);
+    if (fd < 0) {
+      perror("open");
+      return 1;
+    }
+    /* STEP 5: 受信データをクライアントに送り返す */
 
     nfds = epoll_wait(epfd, events, MAX_EVENTS, 3000);
     if (nfds < 0) {
       perror("epoll_wait");
       return 1;
     }
-    if (nfds == 0) {
-      continue;
-    }
-    printf("got epoll event\n");
+    printf("got epoll\n");
+    printf("%d\n", nfds);
+    // if (nfds == 0) {
+    //     printf("timeout\n");
+    //     continue;
+    // }
+
     for (int i = 0; i < nfds; i++) {
       if (events[i].data.fd == sock) {
         addrLen = sizeof(clientAddr);
@@ -97,37 +112,25 @@ int main(int argc, char **argv) {
         if (n < 0) {
           perror("recvfrom");
         }
-        rev_cnt++;
-      }
-      fd = open(filename, O_RDONLY);
-      if (fd < 0) {
-        perror("open");
-        return 1;
-      }
-
-      while ((n = read(fd, buf, BUF_LEN)) > 0) {
-        if (sendto(sock, buf, n, 0, (struct sockaddr *)&clientAddr, addrLen) !=
-            n) {
-          perror("sendto");
-          break;
-        }
-      }
-
-      close(fd);
-      // send eof
-      // this will send 0 byte packet
-      printf("send eof\n");
-      if (sendto(sock, buf, 0, 0, (struct sockaddr *)&clientAddr, addrLen) !=
-          0) {
-        perror("sendto");
-        break;
       }
     }
 
-    printf("\n");
+    printf("start sending\n");
+    while ((n = read(fd, buf, BUF_LEN)) > 0) {
+      if (sendto(sock, buf, n, 0, (struct sockaddr *)&clientAddr, addrLen) !=
+          n) {
+        perror("sendto");
+        break;
+      }
+      usleep(SLEEPTIME);
+    }
+
+    // send eof
+    printf("sent eof\n");
+    sendto(sock, buf, 0, 0, (struct sockaddr *)&clientAddr, addrLen);
   }
 
-  close(sock);
+  close(sock); /* ソケットのクローズ */
 
   return 0;
 }
