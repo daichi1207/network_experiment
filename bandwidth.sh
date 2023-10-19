584e97b064e20741febca58f1f1835f5dae6f92c
daichi1207 <daichiyashima@gmail.com>
2023-10-19 21:32:58 +0900

    fix: bandwidth.sh & added server_bandwidth log

--- bandwidth.sh
+++ bandwidth.sh
@@ -7,29 +7,36 @@ fi
 
 NODES=("node1" "node2" "node3" "node4" "node5")
 
+for NODE in "${NODES[@]}"; do
+    docker exec $NODE pkill iperf3
+    echo "Killed iperf3 processes on $NODE"
+done
+
 LOGFILE="server_bandwidth.log"
 
-# Clear previous log file
 > $LOGFILE
 
 for SERVER in "${NODES[@]}"; do
-    # Start iperf3 server on the server node
-    docker exec -d $SERVER iperf3 -s --json &
-    SERVER_PID=$!
+    docker exec -d $SERVER sh -c "iperf3 -s --json > /tmp/iperf3-logs 2>&1 &"
 
     for CLIENT in "${NODES[@]}"; do
         if [ "$SERVER" != "$CLIENT" ]; then
-            # Initiate iperf3 client connection to server
-            docker exec $CLIENT iperf3 -c $SERVER -b 60M -t 10
+            docker exec $CLIENT iperf3 -c $SERVER -b 60M -t 4
+            sleep 2   
         fi
     done
     
-    SERVER_LOG=$(docker exec $SERVER cat /tmp/iperf3-logs)
-    echo "Server-side bandwidth measurements for $SERVER:" >> $LOGFILE
-    echo "$SERVER_LOG" | jq '.end.sum_received.bits_per_second' >> $LOGFILE
+    SERVER_LOG=$(docker exec $SERVER cat /tmp/iperf3-logs 2>/dev/null)
     
-    docker kill $SERVER_PID
-done
+    if [ ! -z "$SERVER_LOG" ]; then
+        echo "Server-side bandwidth measurements for $SERVER:" >> $LOGFILE
+        echo "$SERVER_LOG" | jq '.end.sum_received.bits_per_second' >> $LOGFILE
+    else
+        echo "Failed to retrieve logs for $SERVER." >> $LOGFILE
+    fi
 
-cat $LOGFILE
+    # Kill iperf3 server process
+    docker exec $SERVER pkill iperf3
+    sleep 2
+done
 
